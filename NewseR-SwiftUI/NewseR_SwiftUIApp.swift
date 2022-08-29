3ccb14a168a7788545fee13fc8250d84498de5dd
Tsahoika Hleb <hlebtsahoika@icloud.com>
2022-08-29 08:54:11 +0300

    Initial Commit

---
+++ NewseR-SwiftUI/NewseR_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewseR_SwiftUIApp.swift
+//  NewseR-SwiftUI
+//
+//  Created by Hleb Tsahoika on 29.08.22.
+//
+
+import SwiftUI
+
+@main
+struct NewseR_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
